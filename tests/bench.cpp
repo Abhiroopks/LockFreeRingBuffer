d34ee0b7e8f493168cda2ba5f0c496db445368b0
Abhiroop Kodandapur Sanjeeva <abhiroopks@gmail.com>
2026-09-21 19:34:37 -0400

    add dequeue benchmark

--- tests/bench.cpp
+++ tests/bench.cpp
@@ -17,6 +17,59 @@ struct Benchmark
 
 // ─── Individual benchmarks ─────────────────────────────────────────────────────
 
+void bench_dequeue()
+{
+    const auto SIZE = (1 << 13);
+    const auto NUM_THREADS = std::thread::hardware_concurrency();
+    const auto TOTAL_ELEMS = SIZE;
+
+    LockFreeRingBuffer<std::string> lfrb(SIZE);
+
+    // Fill the buffer first (single threaded)
+    for (auto i = 0; i < TOTAL_ELEMS; i++) {
+        lfrb.push(std::to_string(i));
+    }
+
+    // Multithreaded pops.
+    std::vector<std::thread> threads;
+    const auto elemsPerThread = TOTAL_ELEMS / NUM_THREADS;
+    std::atomic<bool> startFlag{false};
+
+    for (auto i = 0; i < NUM_THREADS; i++) {
+        threads.emplace_back([i, &lfrb, &startFlag, elemsPerThread]() {
+            // wait for startFlag
+            while (!startFlag.load()) {
+                std::this_thread::yield();
+            }
+
+            std::string val;
+
+            for (auto j = 0; j < elemsPerThread; j++) {
+                lfrb.pop(val);
+            }
+        });
+    }
+
+    startFlag.store(true);
+
+    auto start = std::chrono::steady_clock::now();
+    for (auto &t : threads) {
+        if (t.joinable()) {
+            t.join();
+        }
+    }
+
+    auto end = std::chrono::steady_clock::now();
+    auto diff = end - start;
+
+    double elapsedSeconds = std::chrono::duration<double>(diff).count();
+    double elemsPerSecond = static_cast<double>(TOTAL_ELEMS) / elapsedSeconds;
+
+    std::cout << "Popped " << TOTAL_ELEMS << " elements in "
+              << std::chrono::duration_cast<std::chrono::microseconds>(diff) << " ("
+              << elemsPerSecond << " elems/s) with " << NUM_THREADS << " threads\n";
+}
+
 void bench_enqueue()
 {
     const auto SIZE = (1 << 13);
@@ -60,16 +113,14 @@ void bench_enqueue()
 
     std::cout << "Pushed " << TOTAL_ELEMS << " elements in "
               << std::chrono::duration_cast<std::chrono::microseconds>(diff) << " ("
-              << elemsPerSecond << " elems/s)\n";
+              << elemsPerSecond << " elems/s) with " << NUM_THREADS << " threads\n";
 }
 
 // ─── Main ──────────────────────────────────────────────────────────────────────
 
 int main()
 {
-    std::vector<Benchmark> benchmarks = {
-        {"Enqueue", bench_enqueue},
-    };
+    std::vector<Benchmark> benchmarks = {{"Enqueue", bench_enqueue}, {"Dequeue", bench_dequeue}};
 
     for (auto &b : benchmarks) {
         std::cout << "\n=== " << b.name << " ===\n";
